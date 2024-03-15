49d579f352c51583e674e78a3d66018e13b2410f
ISxaNI <jasonliu99999@yahoo.com>
2024-03-14 18:53:36 -0700

    change

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-    res.send('GET /places')
-})
-
+app.get('/', (req, res) => {
+    res.render('places/index')
+  })
+  
 module.exports = router
