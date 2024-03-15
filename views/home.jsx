9ddb2c1d13953c2a8137dad70e78bca51a817749
ISxaNI <jasonliu99999@yahoo.com>
2024-03-14 20:42:02 -0700

    part 4 partial

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,11 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <img src="./images/Ladle.jpg" alt="ladle noodle" />
+              <p>Photo by Piotr Miazga on Unsplash</p>
+              <a href="/places">
+              <button className="btn-primary">Places Page</button>
+              </a>
           </main>
       </Def>
     )
