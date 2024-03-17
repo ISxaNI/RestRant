517d2075088d96cac62ebc75c9055197f65ded87
ISxaNI <jasonliu99999@yahoo.com>
2024-03-17 14:02:36 -0700

    issues with method-override

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,17 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{ data.place.name }</h1>
+            <h2>Rating</h2>
+            <h3>Not Rated</h3>
+            <h2>Description</h2>
+            <h3>Located in {data.city}, {data.state}</h3>
+            <h2>Comments</h2>
+            <h3>No Comments Yet!</h3>
           </main>
         </Def>
     )
