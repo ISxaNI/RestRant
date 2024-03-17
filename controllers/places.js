3185c863b3127f30d0d6d9e6255eeb7f84c31b15
ISxaNI <jasonliu99999@yahoo.com>
2024-03-17 11:40:14 -0700

    part 5 finished

--- controllers/places.js
+++ controllers/places.js
@@ -1,24 +1,27 @@
 const router = require('express').Router()
-
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/Restaurant.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/Bar.jpg'
-      }]      
-    res.render('./places/index', {places})
-})
+const places = require('../models/places.js')
 
 router.get('/new', (req, res) => {
   res.render('./places/new')
+})
+
+router.get('/', (req, res) => {
+  res.render('places/index', {places })
+})
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  if (!req.body.pic) {
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
 
 module.exports = router
