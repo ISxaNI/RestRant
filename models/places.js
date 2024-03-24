4d3f369fbb5d1130364dae3787afae48c291eb35
ISxaNI <jasonliu99999@yahoo.com>
2024-03-23 19:50:32 -0700

    Finished Part 8, setup website to use MongoDB

--- models/places.js
+++ models/places.js
@@ -1,3 +1,15 @@
+const mongoose = require('mongoose')
+const placeSchema = new mongoose.Schema ({
+    name: {type: String, required: true},
+    pic: String, 
+    cuisines: {type: String, required: true},
+    city: {type: String, required: true},
+    state: {type: String, required: true},
+    founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
+
 module.exports = [{
     name: 'H-Thai-ML',
     city: 'Seattle',
