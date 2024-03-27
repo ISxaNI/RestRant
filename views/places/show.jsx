4a981d8bb45f93f84c0484863a15a88c0c105672
ISxaNI <jasonliu99999@yahoo.com>
2024-03-26 19:15:14 -0700

    changes to helper

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,13 +5,14 @@ function show (data) {
     return (
         <Def>
           <main>
-            <img src={data.place.pic} alt={data.place.name} />
             <h1>{ data.place.name }</h1>
             <h2>Rating</h2>
             <h3>Not Rated</h3>
             <h2>Description</h2>
-            <h3>{data.place.cuisines}</h3>
-            <h3>Located in {data.place.city}, {data.place.state}</h3>
+            <h3>{data.place.showEstablished()}</h3>
+            <h3>Serving {data.place.cuisines}</h3>
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>{Located in {data.place.city}, {data.place.state}}</h3>
             <h2>Comments</h2>
             <h3>No Comments Yet!</h3>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
