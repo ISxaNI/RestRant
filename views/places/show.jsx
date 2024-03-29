92f8067323231e777f689a036500a763e2a4eaab
ISxaNI <jasonliu99999@yahoo.com>
2024-03-28 18:21:32 -0700

    attempt on part 10

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+  let comments = (
+    <h3 className="inactive">
+      No Comments Yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! ' : 'Rave!'} </h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <strong>- {c.author}</strong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    }) 
+  }
     return (
         <Def>
           <main>
@@ -14,7 +33,7 @@ function show (data) {
             <img src={data.place.pic} alt={data.place.name} />
             <h3>Located in {data.place.city}, {data.place.state}</h3>
             <h2>Comments</h2>
-            <h3>No Comments Yet!</h3>
+            {comments}
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
               Edit
             </a>  
